3a4ec37023c60dd4341c93755611ce6dd77371b6
Andrii Zolotarov <anzo0316@netcracker.com>
2016-08-25 16:55:12 +0300

    jar lesson

---
+++ ua/od/anzo/TaskT.java
@@ -0,0 +1,12 @@
+package ua.od.anzo;
+
+
+public class TaskT {
+
+    public static void main(String[] args) {
+
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        System.out.println(x + y);
+    }
+}
